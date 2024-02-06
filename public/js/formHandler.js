6a813790a0b56b750380f456e08b057b6e4f56f1
Pedro Castaneda <petecast76@gmail.com>
2024-02-06 04:12:03 -0800

    feat: can now delete trailers from list

--- public/js/formHandler.js
+++ public/js/formHandler.js
@@ -2,37 +2,43 @@ const form = document.querySelector('#trailer-form');
 const yardId = form.getAttribute('data-yard-id');
 const trailerList = document.querySelector('#trailer-list');
 const addTrailerBtn = document.querySelector('#addTrailerBtn');
-const deleteBtn = document.querySelector('#deleteBtn');
 const submitBtn = document.querySelector('#submitBtn');
 
 let trailers = [];
 
 addTrailerBtn.addEventListener('click', function (e) {
     e.preventDefault();
-    const formData = new FormData(form);
-    console.log(formData);
-    const trailer = {};
-    for (let [key, value] of formData.entries()) {
-        console.log(`In for loop: ${key}: ${value}`);
-        trailer[key] = value;
+    form.classList.remove('was-validated');
+    if (form.checkValidity()) {
+        const formData = new FormData(form);
+        const trailer = {};
+        for (let [key, value] of formData.entries()) {
+            trailer[key] = value;
+        }
+        trailers.push(trailer);
+        updateTrailerList(trailers);
+        form.reset();
+    } else {
+        form.classList.add('was-validated');
     }
-    console.log(trailer);
-    trailers.push(trailer);
-    console.log(trailers);
-    updateTrailerList(trailers);
-    form.reset();
 });
+
+
 
 function updateTrailerList(trailers) {
     trailerList.innerHTML = '';
     trailers.forEach((trailer, index) => {
         const trailerItem = document.createElement('li');
+        trailerItem.className = 'list-group-item-dark d-flex justify-content-between px-3 mb-3';
         trailerItem.innerHTML = `
-        <li class="list-group-item bg-dark text-light border border-0 d-flex justify-content-between px-3">
-        <p class="m-0 fw-bold">${trailer.number}</p>
-        <button id="deleteBtn" class="btn btn-sm btn-danger">Delete</button>
-        </li>
+        <p id="trailerNumber" data-trailer-number="${trailer.number}" class="m-0 fw-bold">${trailer.number}</p>
+        <button class="btn btn-sm btn-danger" type="button">Delete</button>
         `;
+        let deleteBtn = trailerItem.querySelector('.btn-danger');
+        deleteBtn.addEventListener('click', function () {
+            trailers = trailers.filter(t => { return t.number !== trailer.number });
+            updateTrailerList(trailers);
+        });
         trailerList.appendChild(trailerItem);
     });
 };
